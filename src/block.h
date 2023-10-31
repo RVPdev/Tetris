2a93cfd8f945f652eeaa67a67a2ac0c507fbedc2
RVPdev <renatovilcahuaman@gmail.com>
2023-10-30 22:28:50 -0500

    added black class

--- src/block.h
+++ src/block.h
@@ -2,15 +2,18 @@
 #include <vector>
 #include <map>
 #include "position.h"
+#include "colors.h"
+
 
 class Block
 {
 public:
     Block();
     int id;
-    std:map<int, std::vector<Position>> cells;
+    std::map<int, std::vector<Position>> cells;
 private:
     int cellSize;
     int rotationState;
+    std::vector<Color> colors;
 };
 
