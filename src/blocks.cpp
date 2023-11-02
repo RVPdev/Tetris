05bbadf256b185c33203f94cc2002954ac25bdbd
RVPdev <renatovilcahuaman@gmail.com>
2023-11-01 22:24:58 -0500

    added zblock

--- src/blocks.cpp
+++ src/blocks.cpp
@@ -113,7 +113,7 @@ public:
     // Constructor for the OBlock class
     OBlock()
     {
-        id = 3; // Set the block ID to 1
+        id = 4; // Set the block ID to 1
 
         // Define the block cells for rotation state 0
         cells[0] = {Position(0, 0),
@@ -148,7 +148,7 @@ public:
     // Constructor for the SBlock class
     SBlock()
     {
-        id = 3; // Set the block ID to 1
+        id = 5; // Set the block ID to 1
 
         // Define the block cells for rotation state 0
         cells[0] = {Position(0, 1),
@@ -183,7 +183,7 @@ public:
     // Constructor for the TBlock class
     TBlock()
     {
-        id = 3; // Set the block ID to 1
+        id = 6; // Set the block ID to 1
 
         // Define the block cells for rotation state 0
         cells[0] = {Position(0, 1),
@@ -208,5 +208,40 @@ public:
                     Position(1, 0),
                     Position(1, 1),
                     Position(2, 1)};
+    }
+};
+
+// Define a new class ZBlock, inheriting from Block
+class ZBlock : public Block
+{
+public:
+    // Constructor for the ZBlock class
+    ZBlock()
+    {
+        id = 7; // Set the block ID to 1
+
+        // Define the block cells for rotation state 0
+        cells[0] = {Position(0, 0),
+                    Position(0, 1),
+                    Position(1, 1),
+                    Position(1, 2)};
+
+        // Define the block cells for rotation state 1
+        cells[1] = {Position(0, 2),
+                    Position(1, 1),
+                    Position(1, 2),
+                    Position(2, 1)};
+
+        // Define the block cells for rotation state 2
+        cells[2] = {Position(1, 0),
+                    Position(1, 1),
+                    Position(2, 1),
+                    Position(2, 2)};
+
+        // Define the block cells for rotation state 3
+        cells[3] = {Position(0, 1),
+                    Position(1, 0),
+                    Position(1, 1),
+                    Position(2, 0)};
     }
 };
