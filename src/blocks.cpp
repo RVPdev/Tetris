0755d2f470221916e19c6040b5755e59d3e42764
RVPdev <renatovilcahuaman@gmail.com>
2023-11-01 22:03:45 -0500

    added jblock

--- src/blocks.cpp
+++ src/blocks.cpp
@@ -35,3 +35,38 @@ public:
                     Position(2, 1)};
     }
 };
+
+// Define a new class JBlock, inheriting from Block
+class JBlock : public Block
+{
+public:
+    // Constructor for the JBlock class
+    JBlock()
+    {
+        id = 2; // Set the block ID to 1
+
+        // Define the block cells for rotation state 0
+        cells[0] = {Position(0, 0),
+                    Position(1, 0),
+                    Position(1, 1),
+                    Position(1, 2)};
+
+        // Define the block cells for rotation state 1
+        cells[1] = {Position(0, 1),
+                    Position(0, 2),
+                    Position(1, 1),
+                    Position(2, 1)};
+
+        // Define the block cells for rotation state 2
+        cells[2] = {Position(1, 0),
+                    Position(1, 1),
+                    Position(1, 2),
+                    Position(2, 2)};
+
+        // Define the block cells for rotation state 3
+        cells[3] = {Position(0, 1),
+                    Position(1, 1),
+                    Position(2, 0),
+                    Position(2, 1)};
+    }
+};
