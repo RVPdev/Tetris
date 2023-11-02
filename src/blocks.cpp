89cb17f4ad5d9424f105faffc2b532bbc0249d23
RVPdev <renatovilcahuaman@gmail.com>
2023-11-01 22:18:25 -0500

    added sblock

--- src/blocks.cpp
+++ src/blocks.cpp
@@ -140,3 +140,38 @@ public:
                     Position(1, 1)};
     }
 };
+
+// Define a new class SBlock, inheriting from Block
+class SBlock : public Block
+{
+public:
+    // Constructor for the SBlock class
+    SBlock()
+    {
+        id = 3; // Set the block ID to 1
+
+        // Define the block cells for rotation state 0
+        cells[0] = {Position(0, 1),
+                    Position(0, 2),
+                    Position(1, 0),
+                    Position(1, 1)};
+
+        // Define the block cells for rotation state 1
+        cells[1] = {Position(0, 1),
+                    Position(1, 1),
+                    Position(1, 2),
+                    Position(2, 2)};
+
+        // Define the block cells for rotation state 2
+        cells[2] = {Position(1, 1),
+                    Position(1, 2),
+                    Position(2, 0),
+                    Position(2, 1)};
+
+        // Define the block cells for rotation state 3
+        cells[3] = {Position(0, 0),
+                    Position(1, 0),
+                    Position(1, 1),
+                    Position(2, 1)};
+    }
+};
