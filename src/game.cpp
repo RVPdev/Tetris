3b646ac9965d19a664cedc0d529d6720a1c46339
RVPdev <renatovilcahuaman@gmail.com>
2023-11-03 21:26:35 -0500

    added Comments

--- src/game.cpp
+++ src/game.cpp
@@ -1,34 +1,38 @@
-#include "game.h"
-#include <random>
+#include "game.h" // Include the header for the Game class
+#include <random> // Include the random library for generating random numbers
 
-
+// Constructor for the Game class
 Game::Game()
 {
-    grid = Grid();
-    blocks = GetAllBlocks();
-    currentBlock = GetRandomBlock();
-    nextBlock = GetRandomBlock();
+    grid = Grid();                   // Initialize the grid for the game
+    blocks = GetAllBlocks();         // Retrieve all types of blocks available in the game
+    currentBlock = GetRandomBlock(); // Set the current block to a random block
+    nextBlock = GetRandomBlock();    // Set the next block to a random block
 }
 
+// Method to get a random block from the available block types
 Block Game::GetRandomBlock()
 {
-    if(blocks.empty())
+    if (blocks.empty()) // Check if the list of blocks is empty
     {
-        blocks = GetAllBlocks();
+        blocks = GetAllBlocks(); // If empty, refill the list with all block types
     }
-    int randomIndex = rand() % blocks.size();
-    Block block = blocks[randomIndex];
-    blocks.erase(blocks.begin() + randomIndex);
-    return block;
+    int randomIndex = rand() % blocks.size();   // Generate a random index
+    Block block = blocks[randomIndex];          // Get the block at the random index
+    blocks.erase(blocks.begin() + randomIndex); // Remove the selected block from the list
+    return block;                               // Return the randomly selected block
 }
 
+// Method to retrieve all block types for the game
 std::vector<Block> Game::GetAllBlocks()
 {
+    // Return a vector containing one of each type of block
     return {IBlock(), JBlock(), LBlock(), OBlock(), SBlock(), TBlock(), ZBlock()};
 }
 
+// Method to draw the game state
 void Game::Draw()
 {
-    grid.Draw();
-    currentBlock.Draw();
+    grid.Draw();         // Draw the grid
+    currentBlock.Draw(); // Draw the current block
 }
--- src/game.h
+++ src/game.h
@@ -1,18 +1,20 @@
-#pragma once
-#include "grid.h"
-#include "blocks.cpp"
+#pragma once          // Ensures this header file is included only once during compilation
+#include "grid.h"     // Include the Grid class header for grid-related functionalities
+#include "blocks.cpp" // Include the blocks definitions
 
+// Game class encapsulates the main game logic and state
 class Game
 {
 public:
-    Game();
-    Block GetRandomBlock();
-    std::vector<Block> GetAllBlocks();
-    void Draw();
-    Grid grid;
+    Game();                            // Constructor to initialize the game
+    Block GetRandomBlock();            // Method to get a random block from the available types
+    std::vector<Block> GetAllBlocks(); // Method to retrieve all block types for the game
+    void Draw();                       // Method to draw the game state, including the grid and blocks
+
+    Grid grid; // Grid object representing the game board
 
 private:
-    std::vector<Block> blocks;
-    Block currentBlock;
-    Block nextBlock;
+    std::vector<Block> blocks; // Vector of all block types available in the game
+    Block currentBlock;        // The block that is currently being controlled by the player
+    Block nextBlock;           // The next block that will appear after the current block is placed
 };
